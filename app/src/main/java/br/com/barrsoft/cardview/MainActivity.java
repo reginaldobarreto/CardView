32331cc9d2134df9f2e66b792bf4e9afafa88265
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-27 16:31:50 -0300

    adicionado ação aos botoes

--- app/src/main/java/br/com/barrsoft/cardview/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/cardview/MainActivity.java
@@ -3,12 +3,41 @@ package br.com.barrsoft.cardview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ButterKnife.bind(this);
     }
+
+
+    @OnClick(R.id.button1)
+    public void setActionButton1(View view){
+        Toast.makeText(getApplicationContext(),R.string.botao_1,Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.button2)
+    public void setActionButton2(View view){
+        Toast.makeText(getApplicationContext(),R.string.botao_1,Toast.LENGTH_LONG).show();
+    }
+
+
 }
